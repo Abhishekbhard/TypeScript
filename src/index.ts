acd5d0d42c29a2b4739e333d498189502a558c0b
Abhishek <eduandabhi@gmail.com>
2022-10-06 14:56:32 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+class Account {
+  id: number;
+  owner: string;
+  balance: number;
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+  deposite(amount: number) {
+    if (amount <= 0) {
+      throw new Error("Invalid amount");
+    } else {
+      this.balance = amount;
+    }
+  }
+}
+
+let account = new Account(1, "Abhishek", 0);
+account.deposite(100);
+console.log(account.balance);
