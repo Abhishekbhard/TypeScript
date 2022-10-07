69e5b24f1552cdb30b314dff6a2333981b020e3a
Abhishek <eduandabhi@gmail.com>
2022-10-07 16:31:45 +0530

    type mapping

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+interface Product {
+  name: string;
+  price: number;
+}
 
+type ReadOnly<T> = {
+  readonly //index Signature
+  //keyof
+  [Property in keyof T]: T[Property];
+};
+
+type Optional<T> = {
+  [Property in keyof T]?: T[Property];
+};
+
+let product: ReadOnly<Product> = {
+  name: "a",
+  price: 5,
+};
